4882eecc9163a526d74e36be0898c42bc71d7242
BsLinux <35398147+BrowserPercent65@users.noreply.github.com>
2020-04-07 17:13:22 +0300

    2 Kaynak Dosyası Yüklendi
    Daha Fazla Kaynak İçin https://t.me/overclickofficial Katılın

---
+++ src/Form3.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form3
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://t.me/overclickofficial")
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://t.me/overclickdokuman")
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        Process.Start("https://www.youtube.com/channel/UCR-idXrQzkoBha_z6_oFPiw?sub_confirmation=1")
+    End Sub
+End Class
---
+++ src/win32.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class win32
+    <DllImport("kernel32.dll")> Public Shared Function AllocConsole() As Boolean ' Start The Console
+
+    End Function
+    <DllImport("kernel32.dll")> Public Shared Function FreeConsole() As Boolean ' Stop The Console
+
+    End Function
+
+End Class
